3d055b62d107b181058ad5dfa77115412bc0da5a
HEKNOWSME <iranziclaude@hotmail.com>
2024-09-24 22:47:35 +0200

    install helmet and compression for production to heroku

--- app.js
+++ app.js
@@ -3,5 +3,6 @@ const express = require('express');
 const app = express();
 require('./startup/db')();
 require('./startup/routes')(app);
+require('./startup/prod')(app);
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`Blog app listening on port ${port}!`))
